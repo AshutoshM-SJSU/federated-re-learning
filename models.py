5f7038f5da4d7f047f0781755e242a7318504af8
AshutoshM-SJSU <151249388+AshutoshM-SJSU@users.noreply.github.com>
2026-09-23 18:38:29 -0700

    Increase linear layer sizes in model

--- models.py
+++ models.py
@@ -132,8 +132,8 @@ class FEMNISTCNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 7 * 7, 360)
-        self.fc2 = nn.Linear(360, num_classes)
+        self.fc1 = nn.Linear(64 * 7 * 7, 512)
+        self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(F.relu(self.conv1(x), inplace=True))
